86fd229acbbd936179c2ab2231ed585a2030cd5e
Hà Nguyễn <hanguyentien2000@gmail.com>
2025-04-25 18:25:05 +0700

    Reset tracked files to match .gitignore

--- AdminService/Business/Jwt/Token/ITokenStoreHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AdminService.Business.Jwt.Token
-{
-    public interface ITokenStoreHandler
-    {
-        void SaveToken(string userId, string token);
-        bool IsTokenValid(string userId, string token);
-        void RevokeToken(string userId);
-    }
-}
--- AdminService/Business/LoginController/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AdminService.Business.LoginController
-{
-    public class LoginModel
-    {
-        public string Username {  get; set; }
-        public string Password { get; set; }
-    }
-}
